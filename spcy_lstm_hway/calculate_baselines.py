1b4fe9d8247b2d961382366b0e6e5daf738165e4
Mariss Tubelis <mtu@dips.no>
2018-03-20 15:43:06 +0100

    Remove previous files

--- spcy_lstm_hway/calculate_baselines.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def mean_absolute_error(dataset, value):
-    return np.average(np.absolute(dataset - value))
-
-def mean_and_median(dataset):
-    return (np.mean(dataset), np.median(dataset))
-
-
