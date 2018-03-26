e55cd7646eab9e8d8c41a8bb7ff909fb019a1af3
Mariss Tubelis <mtu@dips.no>
2018-03-26 19:34:06 +0200

    Improve fetch_data script

--- fetch_data.py
+++ fetch_data.py
@@ -7,8 +7,10 @@ import shutil
 import xml.etree.ElementTree as et
 
 MAX_RECORDS_PER_REQUEST = 50
+LABELED_DATA_JQL = "timespent > 0 and resolution = 1"
+UNLABELED_DATA_JQL = "timespent <= 0 or timespent is EMPTY or and resolution != 1"
 
-def create_directories(respository_name):
+def create_folder(respository_name):
 
     if not os.path.exists(system_constants.DATA_FOLDER):
         os.makedirs(system_constants.DATA_FOLDER)
@@ -25,37 +27,32 @@ def create_directories(respository_name):
 def get_auth():
 
     authorize = input("Do you want to sign in? (y/n) ") == "y"
+
     if authorize:
         username = input("Username: ")
         api_token = input("API token: ")
     auth = (username, api_token) if authorize else None
 
 def get_number_of_issues(url, auth, jql=""):
+    
     params = {
         "maxResults" : "0",
         "jql" : jql
     }
     response = requests.get(url, params=params, auth=auth)
+
     if response.status_code == 200:
         return response.json().get("total")
-    print("Request was not successful, status code %d was returned" % response.status_code)
+    return 0
 
 def print_issue_counts(url, auth):
 
-    print("Trying to get the number of issues")
+    print("Fetching the number of total issues")
 
     total_issues = get_number_of_issues(url, auth)
-    print("This repository contains %d issues in total." % total_issues)
-
-    total_issues_with_time_spent = get_number_of_issues(url, auth, "timespent > 0")
-    labeling_coverage = total_issues_with_time_spent / total_issues * 100
-    print("Time spent is reported for %d issues and labeling coverage equals %.2f%%." % (total_issues_with_time_spent, labeling_coverage))
-    
-    resolved_issues_with_time_spent = get_number_of_issues(url, auth, "timespent > 0 and resolution = 1") 
-    resolution_percentage = resolved_issues_with_time_spent / total_issues_with_time_spent * 100
-    print("%d (%.2f%%) of the issues with reported time spent are resolved." % (resolved_issues_with_time_spent, resolution_percentage))
-
-    # timespent <= 0 or timespent is EMPTY or resolution != 1
+    total_labeled_issues = get_number_of_issues(url, auth, LABELED_DATA_JQL)
+    labeling_coverage = total_labeled_issues / total_issues * 100 if total_issues > 0 else 0
+    print("This repository contains %d issues in total of which %d (%.2f%%) of the issues are labeled." % (total_issues, total_labeled_issues, labeling_coverage))
 
 def fetch_slice(url, auth, jql, startAt, maxResults):
     params = {
@@ -71,6 +68,9 @@ def fetch_slice(url, auth, jql, startAt, maxResults):
     issues = json_response.get("issues")
     total = json_response.get("total")
 
+    if issues is None:
+        return (None, 0)
+
     issue_fields = [issue.get("fields") for issue in issues]
     return (issue_fields, total)
 
@@ -80,7 +80,7 @@ def save_slice(filename, data_slice):
         tree = et.parse(filename)
         xmlRoot = tree.getroot()
     else:
-        xmlRoot = et.Element("items")
+        xmlRoot = et.Element(system_constants.XML_ROOT_NAME)
         tree = et.ElementTree(xmlRoot)
 
     for datapoint in data_slice:
@@ -109,26 +109,34 @@ def fetch_and_save_issues(target_file, url, auth, jql=""):
 
         startAt = slice_num * MAX_RECORDS_PER_REQUEST
         data_slice, total_issues = fetch_slice(url, auth, jql, startAt, MAX_RECORDS_PER_REQUEST)
-        save_slice(target_file, data_slice)   
+        if total_issues > 0:
+            save_slice(target_file, data_slice)   
 
         records_processed = min(startAt + MAX_RECORDS_PER_REQUEST, total_issues)
-        print("%d (%.2f%%) of %d issues fetched and saved on %s" % (records_processed, records_processed / total_issues * 100, total_issues, target_file))     
+        if records_processed > 0:
+            processed_percentage = records_processed / total_issues * 100
+            print("%d (%.2f%%) of %d issues fetched and saved on %s" % (records_processed, processed_percentage, total_issues, target_file))     
 
-        slice_num = slice_num + 1        
+        slice_num = slice_num + 1
+
+    return total_issues     
 
 def fetch_data(respository_name, url):
 
-    target_folder = create_directories(respository_name)
+    target_folder = create_folder(respository_name)
 
     endpoint_url = system_constants.HTTPS_PREFIX + url + system_constants.JIRA_REST + system_constants.JIRA_SEARCH
     auth = get_auth()
     print_issue_counts(endpoint_url, auth)
 
     labeled_xml_filename = "%s/%s_%s_%s%s" % (target_folder, respository_name, system_constants.LABELED_FILENAME, system_constants.RAW_POSTFIX, system_constants.XML_FILE_EXTENSION)
-    fetch_and_save_issues(labeled_xml_filename, endpoint_url, auth, "timespent > 0")
+    saved_labeled_issues = fetch_and_save_issues(labeled_xml_filename, endpoint_url, auth, LABELED_DATA_JQL)
 
     unlabeled_xml_filename = "%s/%s_%s_%s%s" % (target_folder, respository_name, system_constants.UNLABELED_FILENAME, system_constants.RAW_POSTFIX, system_constants.XML_FILE_EXTENSION)
-    fetch_and_save_issues(unlabeled_xml_filename, endpoint_url, auth, "timespent <= 0 or timespent is EMPTY")
+    saved_unlabeled_issues = fetch_and_save_issues(unlabeled_xml_filename, endpoint_url, auth, UNLABELED_DATA_JQL)
+
+    if saved_labeled_issues + saved_unlabeled_issues > 0:
+        print("%d labeled and %d unlabeled issues from %s were fetched and saved in %s" % (saved_labeled_issues, saved_unlabeled_issues, url, target_folder))
 
 sys_argv_count = len(sys.argv)
 
