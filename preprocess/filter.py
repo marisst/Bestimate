afe6d6069fe5d1ab40bb3a19b619509cd62bd464
Mariss Tubelis <mariss@mariss.no>
2018-05-10 08:38:39 +0200

    Support filtering sentences

--- preprocess/filter.py
+++ preprocess/filter.py
@@ -3,7 +3,8 @@ import json
 import sys
 
 from preprocess import projects
-from utilities import load_data, string_utils
+from utilities import load_data
+from utilities.string_utils import merge_sentences, get_part_strings, word_count
 from utilities.constants import *
 
 class Extreme(Enum):
@@ -19,7 +20,7 @@ def remove_unlabeled_datapoints(data):
 
     labeled_data = [datapoint for datapoint in data if TIMESPENT_FIELD_KEY in datapoint]
     if(len(labeled_data) != len(data)):
-        print("%d (%d%%) of %d datapoints were removed because they were unlabeled" % string_utils.get_part_strings(len(data)-len(labeled_data), len(data)))
+        print("%d (%d%%) of %d datapoints were removed because they were unlabeled" % get_part_strings(len(data)-len(labeled_data), len(data)))
     return labeled_data
 
 def get_unlabeled_datapoints(data):
@@ -33,7 +34,7 @@ def print_extreme(data, extreme):
         % (
             extreme.name,
             extreme_datapoint[TIMESPENT_FIELD_KEY] / SECONDS_IN_HOUR,
-            extreme_datapoint[SUMMARY_FIELD_KEY]))
+            merge_sentences(extreme_datapoint[SUMMARY_FIELD_KEY])))
     return extreme_datapoint[TIMESPENT_FIELD_KEY]
 
 def remove_outliers(data, minimum_timespent_seconds, maximum_timespent_seconds):
@@ -43,8 +44,7 @@ def remove_outliers(data, minimum_timespent_seconds, maximum_timespent_seconds):
         if datapoint[TIMESPENT_FIELD_KEY] >= minimum_timespent_seconds
             and datapoint[TIMESPENT_FIELD_KEY] <= maximum_timespent_seconds]
 
-    print("%d (%.2f%%) of %d datapoints were selected for testing and training"
-        % string_utils.get_part_strings(len(filtered_data), len(data)))
+    print("%d (%.2f%%) of %d datapoints were selected for testing and training" % get_part_strings(len(filtered_data), len(data)))
 
     return filtered_data
 
@@ -54,7 +54,7 @@ def filter_data_by_projects(data, selected_projects):
         return
 
     selected_data = [datapoint for datapoint in data if projects.is_in(datapoint, selected_projects)]
-    print("%d (%.2f%%) of %d datapoints selected" % string_utils.get_part_strings(len(selected_data), len(data)))
+    print("%d (%.2f%%) of %d datapoints selected" % get_part_strings(len(selected_data), len(data)))
 
     return selected_data
 
@@ -63,7 +63,7 @@ def remove_small_projects(data, minimum_project_size):
 
     issue_counts = projects.get_issue_counts(data)
     selected_projects = {issue_count[0] for issue_count in issue_counts if issue_count[1] >= minimum_project_size}
-    print("%d (%.2f%%) of %d projects were selected" % string_utils.get_part_strings(len(selected_projects), len(projects.get(data))))
+    print("%d (%.2f%%) of %d projects were selected" % get_part_strings(len(selected_projects), len(projects.get(data))))
 
     return selected_projects
 
@@ -96,16 +96,14 @@ def even_distribution(data):
                 continue
             evenly_distributed_data.append(d)
 
-    print("%d (%.2f%%) of %d records were selected and an even distribution was created"
-        % string_utils.get_part_strings(len(evenly_distributed_data), len(data)))
+    print("%d (%.2f%%) of %d records were selected and an even distribution was created" % get_part_strings(len(evenly_distributed_data), len(data)))
 
     return evenly_distributed_data
 
 def escape_short_texts(data, minimum_text_length):
 
-    filtered_data = [datapoint for datapoint in data if len(datapoint.get(SUMMARY_FIELD_KEY, "").split()) + len(datapoint.get(DESCRIPTION_FIELD_KEY, "").split()) >= minimum_text_length]
-    print("%d (%.2f%%) of %d records were selected"
-        % string_utils.get_part_strings(len(filtered_data), len(data)))
+    filtered_data = [datapoint for datapoint in data if word_count(datapoint.get(SUMMARY_FIELD_KEY, "")) + word_count(datapoint.get(DESCRIPTION_FIELD_KEY, "")) >= minimum_text_length]
+    print("%d (%.2f%%) of %d records were selected" % get_part_strings(len(filtered_data), len(data)))
     return filtered_data
 
 
--- utilities/string_utils.py
+++ utilities/string_utils.py
@@ -1,3 +1,14 @@
 def get_part_strings(part, total):
 
     return (part, part / total * 100, total)
+
+def merge_sentences(sentences):
+
+    return " ".join(sentences)
+
+def word_count(sentences):
+
+    if sentences is None or len(sentences) == 0:
+        return 0
+
+    return len(merge_sentences(sentences).split())
