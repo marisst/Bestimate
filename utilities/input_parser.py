620ae4448bb22c70ce48087dbf02ea9b5b2a6cb4
Mariss Tubelis <mariss@mariss.no>
2018-05-20 18:24:44 +0200

    fix error in input_parser

--- utilities/input_parser.py
+++ utilities/input_parser.py
@@ -6,14 +6,13 @@ from utilities.constants import *
 
 def select_repositories(repositories_from_input):
 
-    repositories_from_input = repositories_from_input.split(" ")
-
     if len(repositories_from_input) == 0:
         data_folder_subfolders = [entry for entry in os.listdir(DATA_FOLDER) if os.path.isdir("%s/%s" % (DATA_FOLDER, entry))]
         non_dataset_folders = [STATISTICS_FOLDER]
         non_dataset_folders = [x[len(DATA_FOLDER)+1:] for x in non_dataset_folders]
         return [x for x in data_folder_subfolders if x not in non_dataset_folders and not x.isdigit()]
 
+    repositories_from_input = repositories_from_input.split(" ")
     for dataset in repositories_from_input:
         if not os.path.isdir("%s/%s" % (DATA_FOLDER, dataset)):
             print("Dataset %s does not exist" % dataset)
